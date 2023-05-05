9e0abbef5159e14587fd8261631e92e6d7157615
JJ Bussert <jj.bussert@gmail.com>
2023-05-05 10:40:43 -0500

    corrected unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,31 @@
 import { expect, test } from 'vitest';
 import { run } from '../src';
+import fs from 'fs';
 
 test('run function with echo command', async ({ is }) => {
   const result = run('echo "Hello, World!"', '.env', true);
-  expect(result, 'Hello, World!');
+  expect(result).eq('Hello, World!');
+});
+
+test('run function with echo command for environment variable', async ({ is }) => {
+  // arrange
+  const envFile = '.env.test';
+  fs.writeFileSync(envFile, 'UNIT_TEST="Hello World"\n');
+
+  // act
+  const result = run('echo "$UNIT_TEST"', envFile, true);
+  
+  // assert
+  expect(result).eq('Hello World');
+
+  // cleanup
+  fs.unlinkSync(envFile);
 });
 
 test('run function with non-existent command', async ({ is }) => {
   try {
     run('nonexistent-command', '.env', true);
   } catch (error) {
-    expect(error.code, 'ENOENT');
+    expect(error.code).eq('ENOENT');
   }
 });
